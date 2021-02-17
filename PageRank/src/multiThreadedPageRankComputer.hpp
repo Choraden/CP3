a75dc4915f6974da7f9c33f5ffbc771bc8cdb984
Hubert Grochowski <hg417878@students.mimuw.edu.pl>
2021-02-17 12:15:27 +0100

    multiThreaded data init implementation (not yet finished)

--- PageRank/src/multiThreadedPageRankComputer.hpp
+++ PageRank/src/multiThreadedPageRankComputer.hpp
@@ -15,22 +15,42 @@
 
 class MultiThreadedPageRankComputer : public PageRankComputer {
 public:
-    MultiThreadedPageRankComputer(uint32_t numThreadsArg)
-        : numThreads(numThreadsArg) {};
+    MultiThreadedPageRankComputer(uint32_t numThreadsArg) : numThreads(numThreadsArg) {};
 
-    std::vector<PageIdAndRank> computeForNetwork(Network const& /*network*/, double /*alpha*/, uint32_t /*iterations*/, double /*tolerance*/) const
-    {
-        std::vector<PageIdAndRank> result;
-    	return result;
+    std::vector <PageIdAndRank> computeForNetwork(Network const &network, double alpha, uint32_t iterations, double tolerance) const {
+        std::vector <PageIdAndRank> result;
+        std::vector <std::thread> t{numThreads};
+
+        size_t networkSize = network.getSize();
+        uint32_t pagesForThread = networkSize / numThreads;
+
+        for (uint32_t i = 0; i < numThreads; i += pagesForThread) {
+            t[i] = std::thread{generatePagesId, std::ref(network), i, i == numThreads - 1 ? networkSize - 1 : i + pagesForThread - 1};
+        }
+        joinThreads(t);
+
+        return result;
     }
 
-    std::string getName() const
-    {
+    std::string getName() const {
         return "MultiThreadedPageRankComputer[" + std::to_string(this->numThreads) + "]";
     }
 
 private:
     uint32_t numThreads;
+
+    static void generatePagesId(Network const &network, uint32_t p, uint32_t q) {
+        std::vector<Page> pages = network.getPages();
+        for (uint32_t i = p; i < q; i++) {
+            pages[i].generateId(network.getGenerator());
+        }
+    }
+
+    static void joinThreads(std::vector<std::thread> &v) {
+        for (auto &t : v) {
+            t.join();
+        }
+    }
 };
 
 #endif /* SRC_MULTITHREADEDPAGERANKCOMPUTER_HPP_ */
