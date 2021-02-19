26d36199f6d59a440cd15ffae0780a5e85beba3d
Hubert Grochowski <hg417878@students.mimuw.edu.pl>
2021-02-19 19:16:50 +0100

    some multiThreadedPageRankComp implementation

--- PageRank/src/multiThreadedPageRankComputer.hpp
+++ PageRank/src/multiThreadedPageRankComputer.hpp
@@ -20,44 +20,30 @@ public:
 
     std::vector<PageIdAndRank>
     computeForNetwork(Network const &network, double alpha, uint32_t iterations, double tolerance) const {
-        //std::vector <PageIdAndRank> result;
         std::vector<std::thread> t{numThreads};
-        uint32_t networkSize = network.getSize();
-        //uint32_t pagesForThread = networkSize / numThreads;
-
+        std::mutex mut;
         /// ############### NETWORK DATA INIT #################
         std::unordered_map<PageId, PageRank, PageIdHash> pageHashMap;
         std::unordered_map<PageId, uint32_t, PageIdHash> numLinks;
         std::vector<PageId> danglingNodes;
-        std::vector<PageId> Nodes;
+        std::vector<PageId> nodes;
+        std::unordered_map<PageId, std::vector<PageId>, PageIdHash> edges;
 
+
+        for (uint32_t j = 0; j < numThreads; j++) {
+            t[j] = std::thread{generatePagesInfo, std::ref(network),
+                               j, numThreads, std::ref(mut),
+                               std::ref(pageHashMap), std::ref(numLinks),
+                               std::ref(danglingNodes), std::ref(nodes)};
+        }
+
+        joinThreads(t);
 
         for (auto const &page : network.getPages()) {
-            page.generateId(network.getGenerator());
-            pageHashMap[page.getId()] = 1.0 / networkSize;
-        }
-
-
-        for (auto page : network.getPages()) {
-            numLinks[page.getId()] = page.getLinks().size();
-        }
-        
-        for (auto page : network.getPages()) {
-            if (page.getLinks().size() == 0) {
-                danglingNodes.push_back(page.getId());
-            }
-            Nodes.push_back(page.getId());
-        }
-
-
-
-        std::unordered_map<PageId, std::vector<PageId>, PageIdHash> edges;
-        for (auto page : network.getPages()) {
             for (auto link : page.getLinks()) {
                 edges[link].push_back(page.getId());
             }
         }
-
 
         /// ############# ITERATIONS START ###############
         for (uint32_t i = 0; i < iterations; i++) {
@@ -65,11 +51,9 @@ public:
             std::promise<double> danglePromise[numThreads];
             std::future<double> dangleFuture[numThreads];
             double dangleSum = 0;
-            for (uint32_t j = 0; j < numThreads; j++) {
-                dangleFuture[j] = danglePromise[j].get_future();
-            }
 
             for (uint32_t j = 0; j < numThreads; j++) {
+                dangleFuture[j] = danglePromise[j].get_future();
                 t[j] = std::thread{sumDanglingNodes,
                                    std::ref(danglingNodes),
                                    j,
@@ -90,10 +74,7 @@ public:
 
             for (uint32_t j = 0; j < numThreads; j++) {
                 future[j] = promise[j].get_future();
-            }
-
-            for (uint32_t j = 0; j < numThreads; j++) {
-                t[j] = std::thread{rankPages, std::ref(Nodes),
+                t[j] = std::thread{rankPages, std::ref(nodes),
                                    j, numThreads, dangleSum, alpha,
                                    std::ref(edges), std::ref(pageHashMap),
                                    std::ref(previousPageHashMap), std::ref(numLinks), std::ref(promise[j])};
@@ -108,6 +89,7 @@ public:
             for (auto iter : pageHashMap) {
                 result.push_back(PageIdAndRank(iter.first, iter.second));
             }
+
             ASSERT(result.size() == network.getSize(),
                    "Invalid result size=" << result.size() << ", for network" << network);
 
@@ -127,19 +109,40 @@ public:
 private:
     uint32_t numThreads;
 
-    /* seems useless
-    static void generatePagesId(Network const &network, uint32_t p, uint32_t q) {
-        std::vector<Page> pages = network.getPages();
-        for (uint32_t i = p; i < q; i++) {
-            pages[i].generateId(network.getGenerator());
-        }
-    }*/
-
     static void joinThreads(std::vector<std::thread> &v) {
         for (auto &t : v) {
             t.join();
         }
     }
+
+    static void generatePagesInfo(Network const &network, uint32_t i, uint32_t numThreads,
+                                  std::mutex &mut,
+                                  std::unordered_map<PageId, PageRank, PageIdHash> &pageHashMap,
+                                  std::unordered_map<PageId, uint32_t, PageIdHash> &numLinks,
+                                  std::vector<PageId> &danglingNodes,
+                                  std::vector<PageId> &nodes) {
+        const std::vector<Page> &page = network.getPages();
+        std::vector<PageId> pageId;
+        std::vector<uint32_t> linkNum;
+        while (i < page.size()) {
+            page[i].generateId(network.getGenerator());
+            pageId.push_back(page[i].getId());
+            linkNum.push_back(page[i].getLinks().size());
+            i += numThreads;
+        }
+
+        mut.lock();
+        for (i = 0; i < pageId.size(); i++) {
+            pageHashMap[pageId[i]] = 1.0 / network.getSize();
+            numLinks[pageId[i]] = linkNum[i];
+            if (linkNum[i] == 0) {
+                danglingNodes.push_back(pageId[i]);
+            }
+            nodes.push_back(pageId[i]);
+        }
+        mut.unlock();
+    }
+
 
     static void sumDanglingNodes(std::vector<PageId> &danglingNodes,
                                  uint32_t thread, uint32_t numThreads,
